a7c391e4e60e6a2de4c9ba87703996b5b5188dfa
Lucas Uesato <lucax.uesato@gmail.com>
2018-11-24 21:24:57 -0200

    get dados firebase

--- app/src/main/java/Controller/Usuario/loginActivity.java
+++ app/src/main/java/Controller/Usuario/loginActivity.java
@@ -1,47 +1,22 @@
 package Controller.Usuario;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
-import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-
-import android.os.Build;
-import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.text.TextUtils;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import com.br.fatec.sos_professores.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.br.fatec.sos_professores.R;
 
 public class loginActivity extends AppCompatActivity {
 
@@ -76,7 +51,6 @@ public class loginActivity extends AppCompatActivity {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        //updateUI(currentUser);
     }
 
     private boolean isEmailValid(String email) {
--- app/src/main/java/Controller/Usuario/perfilActivity.java
+++ app/src/main/java/Controller/Usuario/perfilActivity.java
@@ -8,19 +8,31 @@ import android.widget.TextView;
 import com.br.fatec.sos_professores.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.Query;
+import Model.Usuario.Usuario;
+
 
 public class perfilActivity extends AppCompatActivity {
 
+    private FirebaseDatabase firebaseDatabase;
     private FirebaseAuth mAuth;
     private TextView mNome;//Chamando o campo da view
-    private String email;
-
+    private String email , nome;
+    private Query databaseReference;
+    Usuario usuario;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfilaluno);
-        mNome = (TextView) findViewById(R.id.txt_nome);
+        mNome = findViewById(R.id.txt_nome);
         mAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
     }
 
     @Override
@@ -29,10 +41,28 @@ public class perfilActivity extends AppCompatActivity {
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
         email = currentUser.getEmail();
+        System.out.println(email);
+        databaseReference = firebaseDatabase.getReference("usuarios").child(mAuth.getCurrentUser().getUid());
+
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                System.out.println(dataSnapshot.getKey());
+                System.out.println(dataSnapshot.getValue());
+                usuario = dataSnapshot.getValue(Usuario.class);
+                //System.out.println(usuario.getNome());
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getCode());
+                System.out.println("o erro é " + databaseError.getMessage());
+            }
+        });
         carregarCampos();
     }
 
+
     public void carregarCampos(){
-        mNome.setText(email);
+        //mNome.setText(usuario.getNome());
     }
 }
