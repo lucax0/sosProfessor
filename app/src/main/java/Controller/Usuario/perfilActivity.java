b8e7531f12bd866c1a28df017f2f9fd19374f276
Lucas Uesato <lucax.uesato@gmail.com>
2018-11-24 13:01:26 -0200

    passando informaçoes do firebase pras outras telas

--- app/src/main/java/Controller/Usuario/perfilActivity.java
+++ app/src/main/java/Controller/Usuario/perfilActivity.java
@@ -3,6 +3,7 @@ package Controller.Usuario;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.br.fatec.sos_professores.R;
 import com.google.firebase.auth.FirebaseAuth;
@@ -11,14 +12,14 @@ import com.google.firebase.auth.FirebaseUser;
 public class perfilActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
-    private EditText mEmail;//Chamando a View
+    private TextView mNome;//Chamando o campo da view
     private String email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_iperfilaluno);
-        mEmail = (EditText) findViewById(R.id.txt_email);
+        setContentView(R.layout.activity_perfilaluno);
+        mNome = (TextView) findViewById(R.id.txt_nome);
         mAuth = FirebaseAuth.getInstance();
     }
 
@@ -28,9 +29,10 @@ public class perfilActivity extends AppCompatActivity {
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
         email = currentUser.getEmail();
+        carregarCampos();
     }
 
     public void carregarCampos(){
-        
+        mNome.setText(email);
     }
 }
