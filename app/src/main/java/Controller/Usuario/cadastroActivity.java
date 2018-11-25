6bd4a7cce90303fe4fa4ccd3480f2efe0de0d024
Lucas Uesato <lucax.uesato@gmail.com>
2018-11-25 15:25:59 -0200

    salvando usuario

--- app/src/main/java/Controller/Usuario/cadastroActivity.java
+++ app/src/main/java/Controller/Usuario/cadastroActivity.java
@@ -1,13 +1,31 @@
 package Controller.Usuario;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 
 import com.br.fatec.sos_professores.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Date;
+
+import Model.Usuario.Usuario;
+
 
 public class cadastroActivity extends AppCompatActivity {
 
@@ -17,6 +35,9 @@ public class cadastroActivity extends AppCompatActivity {
     private RadioGroup mSexoView;
     private EditText mDataView;
     private EditText mTelefoneView;
+
+    private boolean existeUsuario;
+    String identificacaoUsuario;
 
     private FirebaseAuth mAuth;
 
@@ -30,6 +51,50 @@ public class cadastroActivity extends AppCompatActivity {
         mSexoView = findViewById(R.id.rdn_grupS);
         mDataView = findViewById(R.id.txt_nasc);
         mTelefoneView = findViewById(R.id.txt_num);
+    }
 
+    protected void criarModel(){
+        Usuario usuario = new Usuario() {
+        };
+            usuario.setEmail(mEmailView.getText().toString());
+            usuario.setNome(mNomeView.getText().toString());
+            usuario.setCel(mTelefoneView.getText().toString());
+            usuario.setEmail(mEmailView.getText().toString());
+            usuario.setCep("09810360");
+            usuario.setDtNasc(new Date());
+            usuario.setSenha(mPasswordView.getText().toString());
+            criar(usuario);
+
+    }
+
+    protected void criar(final Usuario usuario){
+        mAuth = FirebaseAuth.getInstance();
+        mAuth.createUserWithEmailAndPassword(usuario.getEmail(), usuario.getSenha())
+            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        //Passando infos banco
+                        usuario.setId(mAuth.getCurrentUser().getUid());
+                        FirebaseDatabase database = FirebaseDatabase.getInstance();
+                        DatabaseReference ref = database.getReference("usuarios").child(mAuth.getCurrentUser().getUid());
+                        ref.child("uid").setValue(mAuth.getCurrentUser().getUid());
+                        ref.child("nome").setValue(usuario.getNome());
+                        ref.child("email").setValue(usuario.getEmail());
+                        ref.child("senha").setValue(usuario.getSenha());
+                        ref.child("telefone").setValue(usuario.getCel());
+                        ref.child("cep").setValue(usuario.getCep());
+                        ref.child("dataNasc").setValue(usuario.getDtNasc());
+                        //Chamando view perfil
+                        Intent it = new Intent(cadastroActivity.this, perfilActivity.class);
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        startActivity(it);
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.d("login", "signInWithEmail:failure", task.getException());
+
+                    }
+                }
+            });
     }
 }
--- app/src/main/java/Model/Usuario/Usuario.java
+++ app/src/main/java/Model/Usuario/Usuario.java
@@ -4,7 +4,7 @@ import java.util.Date;
 
 public abstract class Usuario {
 
-    protected int id;
+    protected String id;
     protected String email;
     protected String senha;
     protected String nome;
@@ -13,7 +13,7 @@ public abstract class Usuario {
     protected String cep;
     protected String cel;
 
-    public Usuario(int id, String email, String senha, String nome, Date dtNasc, char sexo, String cep, String cel) {
+    public Usuario(String id, String email, String senha, String nome, Date dtNasc, char sexo, String cep, String cel) {
         this.id = id;
         this.email = email;
         this.senha = senha;
@@ -27,11 +27,11 @@ public abstract class Usuario {
     public Usuario() {
     }
 
-    public int getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(String id) {
         this.id = id;
     }
 
