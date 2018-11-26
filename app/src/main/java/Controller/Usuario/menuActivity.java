a13116af553ec833a7a4e5a54d8542333f047bf3
Lucas Uesato <lucax.uesato@gmail.com>
2018-11-26 19:11:06 -0200

    arrumando chamada de telas menu

--- app/src/main/java/Controller/Usuario/menuActivity.java
+++ app/src/main/java/Controller/Usuario/menuActivity.java
@@ -9,23 +9,24 @@ import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import com.br.fatec.sos_professores.R;
 
 
-public class menuActivity {
+public class menuActivity extends AppCompatActivity {
 
 
     public void btn_perfilOnClick(View v){
-        Intent it = new Intent(perfilActivity.this, menuActivity.class);
+        Intent it = new Intent(menuActivity.this, perfilActivity.class);
         startActivity(it);
 
     }
     public void btn_aulaOnClick(View v){
-        Intent it = new Intent(perfilActivity.this, menuActivity.class);
+        Intent it = new Intent(menuActivity.this, menuActivity.class);
         startActivity(it);
 
     }
     public void btn_professorOnClick(View v){
-        Intent it = new Intent(perfilActivity.this, menuActivity.class);
+        Intent it = new Intent(menuActivity.this, menuActivity.class);
         startActivity(it);
 
     }
