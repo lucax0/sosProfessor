681d3b4f31f0aa58b18a0e327e79c45ac2ff6944
Victor800augusto <victor.augusto800@gmail.com>
2018-11-26 11:15:50 -0200

    Tela menu só aparencia, perfil com mais botões, tem erros estranhos

---
+++ app/src/main/java/Controller/Usuario/menuActivity.java
@@ -0,0 +1,15 @@
+package Controller.Usuario;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+
+public class menuActivity {
+}
