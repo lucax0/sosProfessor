634db28bd9befd8f0caccb31f8863b6b5ba79e81
Lucas Uesato <lucax.uesato@gmail.com>
2018-11-26 20:07:30 -0200

    linkando activity

--- app/src/main/java/Controller/Usuario/cadastroProfActivity.java
+++ app/src/main/java/Controller/Usuario/cadastroProfActivity.java
@@ -1,4 +1,28 @@
 package Controller.Usuario;
 
-public class cadastroProfActivity {
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+
+import com.br.fatec.sos_professores.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Date;
+
+import Model.Usuario.Professor;
+
+public class cadastroProfActivity extends AppCompatActivity {
 }
