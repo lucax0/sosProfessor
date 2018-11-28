f1f87716cffd6d27b51dbeaeb682cf1f0f84a72b
Lucas Uesato <lucax.uesato@gmail.com>
2018-11-27 22:16:34 -0200

    acertando app com a doc

--- app/src/main/java/Controller/Usuario/autenticacaoActivity.java
+++ app/src/main/java/Controller/Usuario/autenticacaoActivity.java
@@ -18,7 +18,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class loginActivity extends AppCompatActivity {
+public class autenticacaoActivity extends AppCompatActivity {
 
     // UI references.
     private EditText mEmailView;
@@ -28,7 +28,7 @@ public class loginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_iautenticacao);
 
         //CARREGANDO VIEW
         mEmailView = (EditText) findViewById(R.id.txt_email);
@@ -103,7 +103,7 @@ public class loginActivity extends AppCompatActivity {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()) {
-                                Intent it = new Intent(loginActivity.this, perfilActivity.class);
+                                Intent it = new Intent(autenticacaoActivity.this, menuActivity.class);
                                 FirebaseUser user = mAuth.getCurrentUser();
                                 startActivity(it);
                             } else {
@@ -117,7 +117,7 @@ public class loginActivity extends AppCompatActivity {
     }
 
     protected void cadastro(){
-        Intent it = new Intent(loginActivity.this, cadastroActivity.class);
+        Intent it = new Intent(autenticacaoActivity.this, cadastroActivity.class);
         startActivity(it);
     }
 }
--- app/src/main/java/Controller/Usuario/menuActivity.java
+++ app/src/main/java/Controller/Usuario/menuActivity.java
@@ -10,25 +10,38 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import com.br.fatec.sos_professores.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class menuActivity extends AppCompatActivity {
 
+    private FirebaseDatabase firebaseDatabase;
+    private FirebaseAuth mAuth;
 
-    public void btn_perfilOnClick(View v){
-        Intent it = new Intent(menuActivity.this, perfilActivity.class);
-        startActivity(it);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfilaluno);
+        mAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        Button btn_sair = findViewById(R.id.btn_logout);
+        btn_sair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(menuActivity.this, autenticacaoActivity.class);
+                startActivity(it);
+            }
+        });
 
-    }
-    public void btn_aulaOnClick(View v){
-        Intent it = new Intent(menuActivity.this, menuActivity.class);
-        startActivity(it);
-
-    }
-    public void btn_professorOnClick(View v){
-        Intent it = new Intent(menuActivity.this, menuActivity.class);
-        startActivity(it);
-
+        Button btn_perfil = findViewById(R.id.btn_perfil);
+        btn_perfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(menuActivity.this, perfilActivity.class);
+                startActivity(it);
+            }
+        });
     }
 
 }
